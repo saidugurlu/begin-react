967c9f24d921af31525bb978abd5d6e720c8995d
saidugurlu <ugurlumsaid@gmx.de>
2022-04-17 20:11:15 +0200

    style, className and jsx used

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function App() {
+//   return <h1>Benim adim Ebruli</h1>;
+// }
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// class App2 extends React.Component {
+//   render() {
+//     return <h1>Benim adim Kurkuli</h1>;
+//   }
+// }
+// ReactDOM.render(<App2 />, document.getElementById("root2"));
+const names = ["Ali ", "Ebruli", "Veli"];
+
 function App() {
-  return <h1>Benim adim Ebruli</h1>;
+  return (
+    <div>
+      {names.map((name) => (
+        <h1 className="aramsam" style={{color: "red", backgroundColor: "Green"}} >{name}</h1>
+      ))}
+    </div>
+  );
 }
+
 ReactDOM.render(<App />, document.getElementById("root"));
