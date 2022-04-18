0715a5bc829f202a399e88353f1edc206ee6a7b4
saidugurlu <ugurlumsaid@gmx.de>
2022-04-18 19:19:27 +0200

    state built in

--- src/components/collapse.js
+++ src/components/collapse.js
@@ -1,24 +1,51 @@
 import React from "react";
 
-const Collapse = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <a
-        className="btn btn-primary p-2 mt-3 mb-1 w-100"
-        data-bs-toggle="collapse"
-        href={"#".concat(props.href)}
-        role="button"
-        aria-expanded="false"
-        aria-controls="collapseExample"
-      >
-        Link with href
-      </a>
+class Collapse extends React.Component {
+  constructor(props) {
+    super();
 
-      <div className="collapse show" id={props.href}>
-         {props.children}
+    this.state = { showContent: true };
+  }
+
+showMore = () =>{
+    this.setState({ showContent: !this.state.showContent });
+}
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.showMore} className="btn btn-primary p-2 mt-3 mb-1 w-100">Link with href</button>
+
+        {
+            this.state.showContent ? ( <div className="collapse show">{this.props.children}</div>) : null
+        }
+
+       
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 export default Collapse;
+
+//   Fonksiyonel component olarak yazim sekli
+// const Collapse = (props) => {
+//   return (
+//     <div>
+//       <a
+//         className="btn btn-primary p-2 mt-3 mb-1 w-100"
+//         data-bs-toggle="collapse"
+//         href={"#".concat(props.href)}
+//         role="button"
+//         aria-expanded="false"
+//         aria-controls="collapseExample"
+//       >
+//         Link with href
+//       </a>
+
+//       <div className="collapse show" id={props.href}>
+//          {props.children}
+//       </div>
+//     </div>
+//   );
+// };
+// export default Collapse;
